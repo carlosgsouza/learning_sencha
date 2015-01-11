ea7a7e531a240c234284e068a529eefa6555f167
carlosgsouza <carlosgsouza@gmail.com>
2015-01-10 17:40:41 -0800

    Showing blog post titles

--- app.js
+++ app.js
@@ -8,15 +8,32 @@ Ext.application({
 
             items: [
                 {
-                    title: 'Home',
-                    iconCls: 'home',
-                    html: [
-                        '',
-                        'Welcome to Sencha Touch',
-                        "We're creating the Getting Started app, which demonstrates how ",
-                        "to use tabs, lists, and forms to create a simple app.",
-                        'Sencha Touch'
-                    ].join("")
+                    xtype: 'nestedlist',
+                    title: 'Blog',
+                    iconCls: 'star',
+                    displayField: 'title',
+
+                    store: {
+                        type: 'tree',
+
+                        fields: [
+                            'title', 'link', 'author', 'contentSnippet', 'content',
+                            {name: 'leaf', defaultValue: true}
+                        ],
+
+                        root: {
+                            leaf: false
+                        },
+
+                        proxy: {
+                            type: 'jsonp',
+                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'responseData.feed.entries'
+                            }
+                        }
+                    }
                 }
             ]
         });
