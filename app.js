d907ca70beead9a9b46a5af33654ad6b8459f745
carlosgsouza <carlosgsouza@gmail.com>
2015-01-10 17:32:30 -0800

    Moving the menu to the bottom

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ Ext.application({
     launch: function() {
         Ext.create("Ext.tab.Panel", {
             fullscreen: true,
+            tabBarPosition: 'bottom',
+
             items: [
                 {
                     title: 'Home',
                     iconCls: 'home',
-                    html: 'Welcome'
+                    html: [
+                        '',
+                        'Welcome to Sencha Touch',
+                        "We're creating the Getting Started app, which demonstrates how ",
+                        "to use tabs, lists, and forms to create a simple app.",
+                        'Sencha Touch'
+                    ].join("")
                 }
             ]
         });
